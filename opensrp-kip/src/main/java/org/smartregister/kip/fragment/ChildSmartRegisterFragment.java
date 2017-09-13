e036d620245edacd6fd3b71a972592265d7bc1f1
Ephraim Muhia <emuhia@ona.io>
2017-09-13 09:52:26 +0300

    Fix overdue count (in progress)
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-kip/src/main/java/org/smartregister/kip/fragment/ChildSmartRegisterFragment.java
+++ opensrp-kip/src/main/java/org/smartregister/kip/fragment/ChildSmartRegisterFragment.java
@@ -420,6 +420,14 @@ public class ChildSmartRegisterFragment extends BaseSmartRegisterFragment implem
         String mainCondition = " (inactive != 'true' and lost_to_follow_up != 'true') AND ( ";
         ArrayList<VaccineRepo.Vaccine> vaccines = VaccineRepo.getVaccines("child");
 
+        if (vaccines.contains(VaccineRepo.Vaccine.bcg2)) {
+            vaccines.remove(VaccineRepo.Vaccine.bcg2);
+        }
+
+        if (vaccines.contains(VaccineRepo.Vaccine.opv4)) {
+            vaccines.remove(VaccineRepo.Vaccine.opv4);
+        }
+
         for (int i = 0; i < vaccines.size(); i++) {
             VaccineRepo.Vaccine vaccine = vaccines.get(i);
             if (i == vaccines.size() - 1) {
