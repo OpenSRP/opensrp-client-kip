94cf0e2fb71f6e6fc39b2aea4fc0480ee5246368
Ephraim Muhia <emuhia@ona.io>
2017-09-18 11:02:40 +0300

    Added adverse event count
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-kip/src/main/java/org/smartregister/kip/service/intent/Moh710IntentService.java
+++ opensrp-kip/src/main/java/org/smartregister/kip/service/intent/Moh710IntentService.java
@@ -6,12 +6,12 @@ import android.util.Log;
 
 import net.sqlcipher.database.SQLiteDatabase;
 
-import org.smartregister.immunization.repository.VaccineRepository;
 import org.smartregister.kip.application.KipApplication;
 import org.smartregister.kip.receiver.Moh710ServiceBroadcastReceiver;
 import org.smartregister.kip.repository.DailyTalliesRepository;
 import org.smartregister.kip.repository.KipEventClientRepository;
 import org.smartregister.kip.service.Moh710Service;
+import org.smartregister.repository.EventClientRepository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,10 +27,6 @@ public class Moh710IntentService extends IntentService {
     private KipEventClientRepository kipEventClientRepository;
     private Moh710Service moh710Service;
 
-    //HIA2 Status
-    private VaccineRepository vaccineRepository;
-    private static final int DAYS_BEFORE_OVERDUE = 10;
-
     public Moh710IntentService() {
         super("Moh710IntentService");
     }
@@ -44,7 +40,7 @@ public class Moh710IntentService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         Log.i(TAG, "Started MOH 710 service");
         try {
-            // Generate daily HIA2 indicators
+            // Generate daily MOH 710 indicators
             generateDailyMohIndicators();
 
             // Send broadcast message
@@ -68,9 +64,6 @@ public class Moh710IntentService extends IntentService {
         dailyTalliesRepository = KipApplication.getInstance().dailyTalliesRepository();
         kipEventClientRepository = KipApplication.getInstance().eventClientRepository();
         moh710Service = new Moh710Service();
-
-        vaccineRepository = KipApplication.getInstance().vaccineRepository();
-
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -78,6 +71,7 @@ public class Moh710IntentService extends IntentService {
         try {
             generateVaccineIndicators();
             generateRecurringServiceIndicators();
+            generateAdverseEffect();
         } catch (Exception e) {
             Log.e(TAG, e.getMessage(), e);
         }
@@ -96,7 +90,7 @@ public class Moh710IntentService extends IntentService {
 
             SQLiteDatabase db = KipApplication.getInstance().getRepository().getWritableDatabase();
             //get previous dates if shared preferences is null meaning reports for previous months haven't been generated
-            String lastProcessedDate = null;// KipApplication.getInstance().context().allSharedPreferences().getPreference(MOH710_VACCINE_LAST_PROCESSED_DATE);
+            String lastProcessedDate = KipApplication.getInstance().context().allSharedPreferences().getPreference(MOH710_VACCINE_LAST_PROCESSED_DATE);
             ArrayList<HashMap<String, String>> reportDates;
             if (lastProcessedDate == null || lastProcessedDate.isEmpty()) {
                 reportDates = kipEventClientRepository.rawQuery(db, PREVIOUS_VACCINE_REPORT_DATES_QUERY.concat(orderByUpdatedAtColumn));
@@ -138,7 +132,7 @@ public class Moh710IntentService extends IntentService {
         try {
             SQLiteDatabase db = KipApplication.getInstance().getRepository().getWritableDatabase();
             //get previous dates if shared preferences is null meaning reports for previous months haven't been generated
-            String lastProcessedDate = null;// KipApplication.getInstance().context().allSharedPreferences().getPreference(MOH710_RECURRING_SERVICE_LAST_PROCESSED_DATE);
+            String lastProcessedDate = KipApplication.getInstance().context().allSharedPreferences().getPreference(MOH710_RECURRING_SERVICE_LAST_PROCESSED_DATE);
             ArrayList<HashMap<String, String>> reportDates;
             if (lastProcessedDate == null || lastProcessedDate.isEmpty()) {
                 reportDates = kipEventClientRepository.rawQuery(db, PREVIOUS_VACCINE_REPORT_DATES_QUERY.concat(orderByUpdatedAtColumn));
@@ -157,16 +151,53 @@ public class Moh710IntentService extends IntentService {
 
                 Map<String, Object> mohReport = moh710Service.generateRecurringServiceIndicators(db, date);
                 dailyTalliesRepository.save(date, mohReport);
-
-                //TODO REMOVE
-                mohReport = moh710Service.generateAdverseEffectIndicators(db, date);
-                dailyTalliesRepository.save(date, mohReport);
             }
         } catch (Exception e) {
             Log.e(TAG, e.getMessage(), e);
         } finally {
             if (lastUpdatedAt != null) {
                 KipApplication.getInstance().context().allSharedPreferences().savePreference(MOH710_RECURRING_SERVICE_LAST_PROCESSED_DATE, lastUpdatedAt);
+            }
+        }
+
+    }
+
+    private void generateAdverseEffect() {
+        String lastUpdatedAt = null;
+        final String eventType = "'AEFI'";
+        final String updatedAtColumn = EventClientRepository.event_column.updatedAt.toString();
+        final String orderByUpdatedAtColumn = " order by " + updatedAtColumn + " asc";
+        final String PREVIOUS_AEFI_REPORT_DATES_QUERY = "select distinct strftime('%Y-%m-%d'," + EventClientRepository.event_column.eventDate.toString() + ") as eventDate, " + updatedAtColumn + " from " + EventClientRepository.Table.event.name() + " where " + EventClientRepository.event_column.eventType.toString() + " = " + eventType;
+        final String MOH710_AEFI_LAST_PROCESSED_DATE = "MOH710_AEFI_LAST_PROCESSED_DATE";
+
+        try {
+            SQLiteDatabase db = KipApplication.getInstance().getRepository().getWritableDatabase();
+            //get previous dates if shared preferences is null meaning reports for previous months haven't been generated
+            String lastProcessedDate = KipApplication.getInstance().context().allSharedPreferences().getPreference(MOH710_AEFI_LAST_PROCESSED_DATE);
+            ArrayList<HashMap<String, String>> reportDates;
+            if (lastProcessedDate == null || lastProcessedDate.isEmpty()) {
+                reportDates = kipEventClientRepository.rawQuery(db, PREVIOUS_AEFI_REPORT_DATES_QUERY.concat(orderByUpdatedAtColumn));
+
+            } else {
+                reportDates = kipEventClientRepository.rawQuery(db, PREVIOUS_AEFI_REPORT_DATES_QUERY.concat(" where " + updatedAtColumn + " > " + lastProcessedDate + orderByUpdatedAtColumn));
+            }
+
+            if (reportDates == null || reportDates.isEmpty()) {
+                return;
+            }
+
+            for (Map<String, String> dates : reportDates) {
+                String date = dates.get(EventClientRepository.event_column.eventDate.toString());
+                lastUpdatedAt = dates.get(updatedAtColumn);
+
+                Map<String, Object> mohReport = moh710Service.generateAdverseEffectIndicators(db, date);
+                dailyTalliesRepository.save(date, mohReport);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        } finally {
+            if (lastUpdatedAt != null) {
+                KipApplication.getInstance().context().allSharedPreferences().savePreference(MOH710_AEFI_LAST_PROCESSED_DATE, lastUpdatedAt);
             }
         }
 
